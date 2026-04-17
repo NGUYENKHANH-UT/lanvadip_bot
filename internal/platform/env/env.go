c0294f68f8b289f1a442a30af1cdc80ff4b30f61
ThanhKhanh <nguyenthanhkhanh2304@gmail.com>
2026-04-18 01:34:07 +0700

    feat: read env varible

---
+++ internal/platform/env/env.go
@@ -0,0 +1,31 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	return value
+}
+
+func GetInt(key string, fallback int) int {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	num, err := strconv.Atoi(value)
+	if err != nil {
+		return fallback
+	}
+
+	return num
+}
